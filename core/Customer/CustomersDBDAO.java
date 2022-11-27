c76a6e09eca9fdb3314841b7e4ed5603acf7ec2f
stlaptop <showtimesubs@gmail.com>
2022-11-27 14:49:19 +0200

    27112022 noon

--- core/Customer/CustomersDBDAO.java
+++ core/Customer/CustomersDBDAO.java
@@ -15,29 +15,34 @@ import Pool.ConnectionPool;
 
 public class CustomersDBDAO implements CustomerDAO {
 
-
     /**
      * Checks is customer exists by sending Query to DB,
      * Checks if any customer in the DB have the same email and password.
-     * 
      * @param Email
      * @param password
      * @return boolean
      * @throws CouponSystemException
      */
-    public boolean isCustomerExists(String Email, String password){
+    public boolean isCustomerExists(String Email, String password) {
         String sql = "SELECT Email FROM Customers WHERE Email = ? AND password = ?";
-        try (Connection con = ConnectionPool.getInstance().getConnection();) {
+        Connection con = ConnectionPool.getInstance().getConnection();
+        try {
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setString(1, Email);
             ps.setString(2, password);
             ResultSet rs = ps.executeQuery();
-            if(rs.next()){
+            if (rs.next()) {
                 return true;
-            } 
+            }
             throw new CouponSystemException(ExceptionMessage.AUTHENTICATION_FAILED.getMessage());
         } catch (SQLException | CouponSystemException e) {
             System.out.println(e.getMessage());
+        } finally {
+            try {
+                ConnectionPool.getInstance().returnConnection(con);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
         return false;
     }
@@ -47,6 +52,7 @@ public class CustomersDBDAO implements CustomerDAO {
      * First send Query to DB to check if any customer in DB has the same email.
      * If not insert the new customer the the DB by sending Query.
      * Return <int> CustomerID OR return if exists.
+     * 
      * @param customer
      * @return int
      * @throws CouponSystemException
@@ -54,15 +60,15 @@ public class CustomersDBDAO implements CustomerDAO {
     public int addCustomer(Customer customer) {
         String sql = "insert into Customers (id, First_Name, Last_Name, Email, password) values(0, ?, ?, ?, ?)";
         String sql2 = "select * from Customers where Email = ?";
+        Connection con = ConnectionPool.getInstance().getConnection();
         try {
-            Connection con = ConnectionPool.getInstance().getConnection();
             PreparedStatement ps = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
             PreparedStatement ps2 = con.prepareStatement(sql2);
             ps2.setString(1, customer.getEmail());
             ResultSet rs2 = ps2.executeQuery();
             if (rs2.next()) {
                 throw new CouponSystemException(ExceptionMessage.EMAIL_ALREADY_EXIST.getMessage());
-            
+
             }
             ps.setString(1, customer.getFirst_Name());
             ps.setString(2, customer.getLast_Name());
@@ -77,6 +83,12 @@ public class CustomersDBDAO implements CustomerDAO {
             return id;
         } catch (SQLException | CouponSystemException e) {
             System.out.println(e.getMessage());
+        } finally {
+            try {
+                ConnectionPool.getInstance().returnConnection(con);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
         return 0;
 
@@ -90,16 +102,16 @@ public class CustomersDBDAO implements CustomerDAO {
      */
     public void updateCutomer(Customer customer) {
         String sql = "update Customers set First_Name = ?, Last_Name = ?, Email = ?, password = ? where id = ?"; // quarry
+        Connection con = ConnectionPool.getInstance().getConnection();
         try {
-            Connection con = ConnectionPool.getInstance().getConnection();
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setString(1, customer.getFirst_Name());
             ps.setString(2, customer.getLast_Name());
             ps.setString(3, customer.getEmail());
             ps.setString(4, customer.getPassword());
             ps.setInt(5, customer.getId());
-            if(!isCustomerExists(customer.getEmail(), customer.getPassword())){
-             return;
+            if (!isCustomerExists(customer.getEmail(), customer.getPassword())) {
+                return;
             }
             String sql2 = "select * from Customers where Email = ?";
             PreparedStatement ps2 = con.prepareStatement(sql2);
@@ -110,73 +122,89 @@ public class CustomersDBDAO implements CustomerDAO {
             }
             ps.executeUpdate();
             System.out.println("Customer has beed updtaed: " + customer);
-        }catch(SQLException| CouponSystemException e){
+        } catch (SQLException | CouponSystemException e) {
             System.out.println(e.getMessage());
+        } finally {
+            try {
+                ConnectionPool.getInstance().returnConnection(con);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
         return;
     }
 
-
-    
-    /** 
+    /**
      * Delete customer from program DB,
      * Identify customer by CustomerID
      * First delete customer coupons by {@link #deleteCutomerCoupons(int)}
      * Then send Query to DB to delete the Customer.
+     * 
      * @param CustomerID
      * @throws CouponSystemException
      */
-    public void deleteCutomer(int CustomerID){
+    public void deleteCutomer(int CustomerID) {
         String sql = "delete from Customers where id = ?";
+        Connection con = ConnectionPool.getInstance().getConnection();
         try {
-            Connection con = ConnectionPool.getInstance().getConnection();
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, CustomerID);
             deleteCutomerCoupons(CustomerID);
             ps.executeUpdate();
-            System.out.println("The Customer has been deleted. " + "("+ CustomerID + ")");
-        }catch(SQLException| CouponSystemException e){
+            System.out.println("The Customer has been deleted. " + "(" + CustomerID + ")");
+        } catch (SQLException e) {
             System.out.println(ExceptionMessage.CUSTOMER_NOT_EXIST.getMessage());
+        } finally {
+            try {
+                ConnectionPool.getInstance().returnConnection(con);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
-
-    
-    /** 
+    /**
      * Delete customer coupons by sending Query to program DB.
      * Identify customer by CustomerID.
+     * 
      * @param CustomerID
      * @throws CouponSystemException
      */
-    private void deleteCutomerCoupons(int CustomerID){
+    private void deleteCutomerCoupons(int CustomerID) {
         String sql = "delete from customers_vs_coupons where Customer_id = ?";
+        Connection con = ConnectionPool.getInstance().getConnection();
         try {
-            Connection con = ConnectionPool.getInstance().getConnection();
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, CustomerID);
             ps.executeUpdate();
             System.out.println("Customer (" + CustomerID + ") Coupons has been deleted.");
-        }catch(SQLException| CouponSystemException e){
+        } catch (SQLException e) {
             System.out.println(ExceptionMessage.GENERAL_ERROR.getMessage());
+        } finally {
+            try {
+                ConnectionPool.getInstance().returnConnection(con);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
-    
-    
     /**
      * Get customer coupons by Price limit.
      * Send Query to program DB, Identify customer by CustomerID to get all customer
      * coupons.
      * Sort return coupons by Price limit <double>
      * Return <List> OR null if not exists.
+     * 
      * @param customerID
      * @param Price
      * @return List<Coupon> || null
      * @throws CouponSystemException
      */
-    public List<Coupon> getCouponsByPrice(int customerID, double Price){
+    public List<Coupon> getCouponsByPrice(int customerID, double Price) {
         String sql = "Select * from coupons INNER JOIN customers_vs_coupons ON coupons.id = customers_vs_coupons.Coupon_id where Customer_id = ? AND coupons.price < ?";
-        try (Connection con = ConnectionPool.getInstance().getConnection();) {
+        Connection con = ConnectionPool.getInstance().getConnection();
+        try {
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, customerID);
             ps.setDouble(2, Price);
@@ -198,21 +226,26 @@ public class CustomersDBDAO implements CustomerDAO {
             }
             System.out.println(coupons.toString());
             return coupons;
-        }catch(SQLException| CouponSystemException e){
+        } catch (SQLException e) {
             System.out.println(sql);
             System.out.println(ExceptionMessage.GENERAL_ERROR.getMessage());
+        } finally {
+            try {
+                ConnectionPool.getInstance().returnConnection(con);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
         return null;
     }
 
-
-    
     /**
      * Get coustomer coupons by category.
      * Send Query to program DB, Identify customer by CustomerID to get all customer
      * coupons.
      * Sort return coupons by CategoryID <int>
      * Return <List> OR null if not exists.
+     * 
      * @param customerID
      * @param categoryID
      * @return List<Coupon>
@@ -220,7 +253,8 @@ public class CustomersDBDAO implements CustomerDAO {
      */
     public List<Coupon> getCouponsByCategory(int customerID, int categoryID) {
         String sql = "Select * from coupons INNER JOIN customers_vs_coupons ON coupons.id = customers_vs_coupons.Coupon_id where Customer_id = ? AND coupons.category_id = ?";
-        try (Connection con = ConnectionPool.getInstance().getConnection();) {
+        Connection con = ConnectionPool.getInstance().getConnection();
+        try {
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, customerID);
             ps.setInt(2, categoryID);
@@ -242,25 +276,31 @@ public class CustomersDBDAO implements CustomerDAO {
             }
             System.out.println(coupons.toString());
             return coupons;
-        }catch(SQLException| CouponSystemException e){
+        } catch (SQLException e) {
             System.out.println(ExceptionMessage.GENERAL_ERROR.getMessage());
+        } finally {
+            try {
+                ConnectionPool.getInstance().returnConnection(con);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
         return null;
     }
 
-
-    
     /**
      * Get coustomer coupons by category.
      * Send Query to program DB, Identify customer by CustomerID to get all customer
      * coupons.
      * Return <List> OR null if not exists.
+     * 
      * @param customerID
      * @return List<Coupon>
      */
-    public List<Coupon> getCustomerCoupons(int customerID){
+    public List<Coupon> getCustomerCoupons(int customerID) {
         String sql = "Select * from coupons INNER JOIN customers_vs_coupons ON coupons.id = customers_vs_coupons.Coupon_id where Customer_id = ? ";
-        try (Connection con = ConnectionPool.getInstance().getConnection();) {
+        Connection con = ConnectionPool.getInstance().getConnection();
+        try {
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, customerID);
             ResultSet rs = ps.executeQuery();
@@ -281,23 +321,28 @@ public class CustomersDBDAO implements CustomerDAO {
             }
             System.out.println(coupons.toString());
             return coupons;
-        }catch(SQLException| CouponSystemException e){
+        } catch (SQLException e) {
             System.out.println(ExceptionMessage.GENERAL_ERROR.getMessage());
+        } finally {
+            try {
+                ConnectionPool.getInstance().returnConnection(con);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
         return null;
     }
 
-
-    
-    /** 
+    /**
      * Get all customer exists in program DB.
      * Return <List> OR null if not exists
+     * 
      * @return List<Customer>
      */
-    public List<Customer> getAllCustomers(){
+    public List<Customer> getAllCustomers() {
         String sql = "select * from Customers";
+        Connection con = ConnectionPool.getInstance().getConnection();
         try {
-            Connection con = ConnectionPool.getInstance().getConnection();
             PreparedStatement ps = con.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             List<Customer> customers = new ArrayList<>();
@@ -312,15 +357,19 @@ public class CustomersDBDAO implements CustomerDAO {
             }
             System.out.println(customers.toString());
             return customers;
-        }catch(SQLException| CouponSystemException e){
+        } catch (SQLException e) {
             System.out.println(ExceptionMessage.GENERAL_ERROR.getMessage());
+        } finally {
+            try {
+                ConnectionPool.getInstance().returnConnection(con);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
         return null;
     }
 
-
-    
-    /** 
+    /**
      * Get one coustomer.
      * Send Query to program DB, Identify customer by CustomerID to get customer
      * details.
@@ -329,10 +378,10 @@ public class CustomersDBDAO implements CustomerDAO {
      * @return Customer
      * @throws CouponSystemException
      */
-    public Customer getOneCustomer(int CustomerID){
+    public Customer getOneCustomer(int CustomerID) {
         String sql = "select * from Customers where id = ?";
+        Connection con = ConnectionPool.getInstance().getConnection();
         try {
-            Connection con = ConnectionPool.getInstance().getConnection();
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, CustomerID);
             ResultSet rs = ps.executeQuery();
@@ -344,14 +393,19 @@ public class CustomersDBDAO implements CustomerDAO {
                 customer.setEmail(rs.getString("Email"));
                 System.out.println(customer);
                 return customer;
-            } else{ 
+            } else {
                 throw new CouponSystemException(ExceptionMessage.CUSTOMER_NOT_EXIST.getMessage());
             }
         } catch (SQLException | CouponSystemException e) {
             System.out.println(e.getMessage());
+        } finally {
+            try {
+                ConnectionPool.getInstance().returnConnection(con);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
         return null;
     }
-
 
 }
